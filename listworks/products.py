9ffc545be69d2e6843dd551786e6d97ee6aecc71
sreya <sreyavadassery@gmail.com>
2022-06-13 11:28:11 +0530

    new

--- listworks/products.py
+++ listworks/products.py
@@ -13,3 +13,32 @@ mobiles=[
 
 ]
 
+# no of mobiles
+# print(f"total={len(mobiles)}")
+#
+#  total out of stock
+# out_of_stock=[mob for mob in mobiles if mob[-1]==0]
+# print(out_of_stock)
+
+# total stock
+# avl_stock=[mob[-1] for mob in mobiles]
+# print(sum(avl_stock))
+
+# mobiles available between 20000 and 30000
+# mobil_gt=[mob for mob in mobiles if mob[4] in range(20000,30001)]
+# print(mobil_gt)
+
+# print 5g mobiles
+# mobiles_fiveg=[mob for mob in mobiles if mob[2]=="5g"]
+# print(mobiles_fiveg)
+
+# costly mobiles
+# mobiles.sort(reverse=True,key=lambda mob:mob[4])
+# print(mobiles)
+
+# costly_mobile=max(mobiles,key=lambda m:m[4])
+# print(costly_mobile)
+
+# is there any mobile available at 10000
+mobile_ten=[mob[4]==27000 for mob in mobiles]
+print("available" if True in mobile_ten else "not available")
---
+++ tupple_works/item_assign.py
@@ -0,0 +1,4 @@
+expenses=(1000,2222,3000,4000)
+# print(expenses)
+expenses[-1]=5000
+print(expenses)
