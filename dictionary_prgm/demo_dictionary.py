32af808180f0dc54cc65bdef2b88bfddbf2663c4
sreya <sreyavadassery@gmail.com>
2022-06-14 11:32:09 +0530

    new

---
+++ dictionary_prgm/demo_dictionary.py
@@ -0,0 +1,11 @@
+student={"name":"ariya","roll no":20,"class":7,"division":"a"}
+# print(student["name"])
+# print(student["class"])
+# print(student["division"])
+# print("school" in student)
+# student["school"]="toch"
+# print(student)
+# student["mark"]=20
+# print(student)
+student["roll no"]=30
+print(student)
---
+++ tupple_works/demo_set.py
@@ -0,0 +1,22 @@
+# lst=[10,20,30,30,30,10,50]
+# st=set(lst)
+# print(st)
+
+# st1={1,2,3,4}
+# st2={10,11,12,2,3}
+# union_set=st1.union(st2)
+# print(union_set)
+#
+# intersection_set=st1.intersection(st2)
+# print(intersection_set)
+#
+# difference_set=st1.difference(st2)
+# print(difference_set)
+
+# students=["ram","ravi","hari","tom","nikhil","jain","john"]
+# passed_students=["ravi","hari","tom"]
+# st=set(students)
+# pas_st=set(passed_students)
+# failed_students=st-pas_st
+# print(failed_students)
+
